f8835197992c8bc2f07a51545d050cba7b12a4a9
isakhno <sakhno.yaroslav.13.95@gamil.com>
2025-05-21 12:30:32 +0600

    first commit

---
+++ src/main/java/sakhno/psup/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package sakhno.psup.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Сервис запускает eureka сервер
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
